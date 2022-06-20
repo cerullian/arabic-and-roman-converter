59dbc2482d84b87653a68611b0646781ca9f684f
Daniela Patrício <danielaccpatricio@gmail.com>
2022-06-20 19:35:48 +0100

    Add error message

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ const romantonum = document.querySelector("#romantonum");
 const numberconvert = document.querySelector("#numberconvert");
 
 numtoroman.addEventListener("keyup", e => {
-    // console.log(typeof parseInt(e.target.value));
-    const torom = romanize(parseInt(e.target.value));
-    // console.log(torom);
-    romanconvert.innerHTML = `Roman: ${torom}`;
+    try {
+        // console.log(typeof parseInt(e.target.value));
+        const torom = romanize(parseInt(e.target.value));
+        // console.log(torom);
+        romanconvert.innerHTML = `Roman: ${torom}`;
+    }
+    catch(err) {
+        romanconvert.innerHTML = err.message;
+    }    
 })
 
 romantonum.addEventListener("keyup", e => {
-    const tonum = deromanize(e.target.value.toUpperCase());
-    numberconvert.innerHTML = `Arabic: ${tonum}`;
+    try {
+        const tonum = deromanize(e.target.value.toUpperCase());
+        numberconvert.innerHTML = `Arabic: ${tonum}`;
+    }
+    catch(err) {
+        numberconvert.innerHTML = err.message;
+    }
+    
 })
